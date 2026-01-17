30260f8fce90774e0976c2ec1b2eb6473e55f7a2
Daniel <d.h.janse@utwente.nl>
2026-01-17 10:31:34 +0100

    Added Keithly4200A.py v0.1

---
+++ instruments/Keithley4200A.py
@@ -0,0 +1,187 @@
+# -*- coding: utf-8 -*-
+"""
+Module to interact with a Keithley 4200A-SCS Parameter Analyzer.
+Uses pyVISA to communicate with the GPIB device.
+Assumes GPIB address is of the form GPIB0::<xx>::INSTR where
+<xx> is the device address (number).
+
+Version 0.1 (2025-02-19)
+Daan Wielens - Researcher at ICE/QTM
+Daniel Janse van Rensburg - PhD Candidate at ICE
+University of Twente
+d.h.wielens@utwente.nl
+d.h.janse@utwente.nl
+"""
+
+import pyvisa as visa
+
+class WrongInstrErr(Exception):
+    """
+    A connection was established to the instrument, but the instrument
+    is not a Keithley 4200A-SCS. Please retry with the correct
+    GPIB address. Make sure that each device has a unique address.
+    """
+    pass
+
+class Keithley4200A:
+    type = 'Keithley 4200A-SCS Parameter Analyzer'
+
+    def __init__(self, GPIBaddr):
+        rm = visa.ResourceManager()
+        self.visa = rm.open_resource('GPIB0::{}::INSTR'.format(GPIBaddr))
+        resp = self.visa.query('*IDN?')
+        model = resp.split(',')[1]
+        if model not in ['KI4200A', 'MODEL 4200A']:
+            raise WrongInstrErr('Expected Keithley KI4200A, got {}'.format(resp))
+        self.Icomp = 1.0E-3
+        self.Vcomp = 10.0
+        self.Icomp_b = 1.0E-3
+        self.Vcomp_b = 10.0
+
+    def get_iden(self):
+        resp = str(self.visa.query('*IDN?'))
+        return resp
+
+    def close(self):
+        self.visa.close()
+
+    def query(self, val):
+        resp = self.visa.query(val).strip('\n')
+        return resp
+
+    # Normal functions apply to SMU A(1) and _b functions apply to SMU B(2)
+    # This keeps 1:1 relationship with 2400 SMU code
+    
+    def read_dcv(self):
+        self.visa.write('US')
+        resp = self.visa.query('TV 1').strip('\n\r')
+        resp = float(resp[3:64])
+        return resp
+
+    def read_dcv_b(self):
+        self.visa.write('US')
+        resp = self.visa.query('TV 2').strip('\n\r')
+        resp = float(resp[3:64])
+        return resp
+
+    def write_dcv(self, val):
+        fval = float(val)
+        if abs(fval) > 210:
+            print('Your setpoint is higher than the allowed +/- 210 V and will not be applied.')
+        else:
+            self.visa.write('US')
+            self.visa.write('DV1, 0, ' + str(val) + ', ' + str(self.Icomp))
+
+    def write_dcv_b(self, val):
+        fval = float(val)
+        if abs(fval) > 210:
+            print('Your setpoint is higher than the allowed +/- 210 V and will not be applied.')
+        else:
+            self.visa.write('US')
+            self.visa.write('DV2, 0, ' + str(val) + ', ' + str(self.Icomp_b))
+
+    def read_dci(self):
+        self.visa.write('US')
+        resp = self.visa.query('TI 1').strip('\n\r')
+        resp = float(resp[3:64])
+        return resp
+
+    def read_dci_b(self):
+        self.visa.write('US')
+        resp = self.visa.query('TI 2').strip('\n\r')
+        resp = float(resp[3:64])
+        return resp
+
+    def write_dci(self, val):
+        fval = float(val)
+        if abs(fval) > 0.1050:
+            print('Your setpoint is higher than the allowed +/- 105 mA and will not be applied.')
+        else:
+            self.visa.write('US')
+            self.visa.write('DI1, 0, ' + str(val) + ', ' + str(self.Vcomp))
+
+    def write_dci_b(self, val):
+        fval = float(val)
+        if abs(fval) > 0.1050:
+            print('Your setpoint is higher than the allowed +/- 105 mA and will not be applied.')
+        else:
+            self.visa.write('US')
+            self.visa.write('DI2, 0, ' + str(val) + ', ' + str(self.Vcomp))
+
+    def read_i(self):
+        resp = self.read_dci()
+        return resp
+
+    def read_v(self):
+        resp = self.read_dcv()
+        return resp
+
+    def write_Vrange(self, val):
+        if val in ['AUTO', 'auto', 'Autorange', 'autorange']:
+            self.visa.write('SOUR:VOLT:RANG AUTO\n')
+        elif val in ['MAX', 'max', 'maximum', '210']:
+            self.visa.write('SOUR:VOLT:RANG MAX\n')
+        elif val in ['DEF', 'def', 'default', '21']:
+            self.visa.write('SOUR:VOLT:RANG DEF\n')
+        elif val in ['MIN', 'min', 'minimum']:
+            self.visa.write('SOUR:VOLT:RANG MIN\n')
+            
+    def write_Irange(self, val):
+        if val in ['AUTO', 'auto', 'Autorange', 'autorange']:
+            self.visa.write('SOUR:CURR:RANG AUTO\n')
+        elif val in ['MAX', 'max', 'maximum', '1.05']:
+            self.visa.write('SOUR:CURR:RANG MAX\n')
+        elif val in ['DEF', 'def', 'default', '100E-6']:
+            self.visa.write('SOUR:CURR:RANG DEF\n')
+        elif val in ['MIN', 'min', 'minimum', '1E-6']:
+            self.visa.write('SOUR:CURR:RANG MIN\n')
+        else :
+            self.visa.write('SOUR:CURR:RANG ' + str(val) + '\n')
+
+    def read_output(self):
+        resp = int(self.visa.query('OUTP?').strip('\n'))
+        return resp
+
+    def write_output(self, val):
+        if val in [1, 'On', 'ON', 'on']:
+            self.visa.write('OUTP 1\n')
+        elif val in [0, 'Off', 'OFF', 'off']:
+            self.visa.write('OUTP 0\n')
+        else:
+            print('This is not a valid argument for the Keithley Output command. Your command will be ignored.')
+
+    def read_Vcomptrip(self):
+        # When sourcing current, this returns 1 if the voltage is above the compliance limit and 0 otherwise.
+        resp = int(self.visa.query('SENS:VOLT:PROT:TRIP?').strip('\n'))
+        return resp
+    
+    def read_Icomptrip(self):
+        resp = int(self.visa.query('SENS:CURR:PROT:TRIP?').strip('\n'))
+        return resp
+
+    def read_Vcomplevel(self):
+        return self.Vcomp
+
+    def read_Icomplevel(self):
+        return self.Icomp
+
+    def write_Vcomplevel(self, val):
+        self.Vcomp = val
+
+    def write_Icomplevel(self, val):
+        self.Icomp = val
+
+    def read_Vcomplevel_b(self):
+        return self.Vcomp_b
+
+    def read_Icomplevel_b(self):
+        return self.Icomp_b
+
+    def write_Vcomplevel_b(self, val):
+        self.Vcomp_b = val
+
+    def write_Icomplevel_b(self, val):
+        self.Icomp_b = val
+        
+    def write_integT(self, val):
+        self.visa.write('IT' + str(val))
