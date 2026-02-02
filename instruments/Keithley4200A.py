9fdf9b81d5708763c3082efddcd95ec7c61c444c
Daniel <d.h.janse@utwente.nl>
2026-02-02 14:28:44 +0100

    Added usermode commands

--- instruments/Keithley4200A.py
+++ instruments/Keithley4200A.py
@@ -5,7 +5,7 @@ Uses pyVISA to communicate with the ethernet device.
 Assumes IP address is of the form TCPIP0::<xx>::SOCKET where
 <xx> is the IP address (string).
 
-Version 0.5 (2026-01-28)
+Version 0.6 (2026-02-02)
 Daan Wielens - Researcher at ICE/QTM
 Daniel Janse van Rensburg - PhD Candidate at ICE
 University of Twente
@@ -14,6 +14,9 @@ d.h.janse@utwente.nl
 """
 
 import pyvisa
+import numpy as np
+from enum import Enum, nonmember
+
 
 class WrongInstrErr(Exception):
     """
@@ -26,6 +29,14 @@ class WrongInstrErr(Exception):
 class Keithley4200A:
     type = 'Keithley 4200A-SCS Parameter Analyzer'
 
+    class Pages(Enum):
+        CHANNEL_SET = "CH"
+        SOURCE_SET = "SS"
+        MEAS_SET = "SM"
+        MEAS_CON = "MD"
+        USER = "US"
+        USER_LIB = "UL"
+
     def __init__(self, IPAddr):
         rm = pyvisa.ResourceManager()
         self.visa = rm.open_resource('TCPIP0::{}::1225::SOCKET'.format(IPAddr))
@@ -35,6 +46,7 @@ class Keithley4200A:
         model = resp.split(',')[1]
         if model not in ['KI4200A', 'MODEL 4200A']:
             raise WrongInstrErr('Expected Keithley KI4200A, got {}'.format(resp))
+
         self.Icomp = 1.0E-3
         self.Vcomp = 10.0
         self.Icomp_b = 1.0E-3
@@ -66,20 +78,12 @@ class Keithley4200A:
         self.Irange_b = self.IRanges.get("auto")
         self.Vrange = self.VRanges.get("auto")
         self.Vrange_b = self.VRanges.get("auto")
-        self.Pages = {
-            "ChannelDef": "CH",
-            "SourceSetup": "SS",
-            "MeasSetup": "SM",
-            "MeasCont": "MD",
-            "UserMode": "US",
-            "UserLib": "UL",
-        }
-        self.page = None
-        self.visa.query('DR1')
 
-    def get_iden(self):
-        resp = str(self.visa.query('*IDN?'))
-        return resp
+        self.page = None
+        self.holdt = 0
+        self.delayt = 0
+
+        self.visa.query('DR1')
 
     def close(self):
         self.visa.close()
@@ -88,20 +92,10 @@ class Keithley4200A:
         resp = self.visa.query(val)
         return resp
 
-    def pol(self, val):
-        self.visa.write(val)
-        if self.stb.pol(0):
-            resp = self.visa.read()
-            return resp
-
-    def set_page(self, val):
-        newpage = self.Pages.get(val)
-        if newpage is None:
-            print(f'Unknown page: {val}; Current page: {self.page}')
-            return False
-        if newpage == self.page:
+    def set_page(self, page):
+        if page == self.page:
             return True
-        self.page = newpage
+        self.page = page
         self.visa.query(self.page)
         return True
 
@@ -109,13 +103,13 @@ class Keithley4200A:
     # This keeps 1:1 relationship with 2400 SMU code
 
     def read_dcv(self):
-        self.set_page("UserMode")
+        self.set_page(self.Pages.USER)
         resp = self.visa.query('TV 1')
         resp = float(resp[3:64])
         return resp
 
     def read_dcv_b(self):
-        self.set_page("UserMode")
+        self.set_page(self.Pages.USER)
         resp = self.visa.query('TV 2')
         resp = float(resp[3:64])
         return resp
@@ -125,7 +119,7 @@ class Keithley4200A:
         if abs(fval) > 210:
             print('Your setpoint is higher than the allowed +/- 210 V and will not be applied.')
         else:
-            self.set_page("UserMode")
+            self.set_page(self.Pages.USER)
             self.visa.query('DV1, ' + str(self.Vrange) + ', ' + str(val) + ', ' + str(self.Icomp))
 
     def write_dcv_b(self, val):
@@ -133,17 +127,17 @@ class Keithley4200A:
         if abs(fval) > 210:
             print('Your setpoint is higher than the allowed +/- 210 V and will not be applied.')
         else:
-            self.set_page("UserMode")
+            self.set_page(self.Pages.USER)
             self.visa.query('DV2, ' + str(self.Vrange_b) + ', ' + str(val) + ', ' + str(self.Icomp_b))
 
     def read_dci(self):
-        self.set_page("UserMode")
+        self.set_page(self.Pages.USER)
         resp = self.visa.query('TI 1')
         resp = float(resp[3:64])
         return resp
 
     def read_dci_b(self):
-        self.set_page("UserMode")
+        self.set_page(self.Pages.USER)
         resp = self.visa.query('TI 2')
         resp = float(resp[3:64])
         return resp
@@ -153,7 +147,7 @@ class Keithley4200A:
         if abs(fval) > 0.1050:
             print('Your setpoint is higher than the allowed +/- 105 mA and will not be applied.')
         else:
-            self.set_page("UserMode")
+            self.set_page(self.Pages.USER)
             self.visa.query('DI1, ' + str(self.Irange) + ', ' + str(val) + ', ' + str(self.Vcomp))
 
     def write_dci_b(self, val):
@@ -161,7 +155,7 @@ class Keithley4200A:
         if abs(fval) > 0.1050:
             print('Your setpoint is higher than the allowed +/- 105 mA and will not be applied.')
         else:
-            self.set_page("UserMode")
+            self.set_page(self.Pages.USER)
             self.visa.query('DI2, ' + str(self.Irange_b) + ', ' + str(val) + ', ' + str(self.Vcomp))
 
     def read_i(self):
@@ -225,7 +219,7 @@ class Keithley4200A:
             return
         elif val in [0, 'Off', 'OFF', 'off']:
             # NOTE: This is a hack to turn off the devices; there is no alternative to switch the 4200 off
-            self.set_page("UserMode")
+            self.set_page(self.Pages.USER)
             self.visa.query("DV1 DV2")
         else:
             print('This is not a valid argument for the Keithley Output command. Your command will be ignored.')
@@ -256,3 +250,102 @@ class Keithley4200A:
 
     def write_integT(self, val):
         self.visa.query('IT' + str(val))
+
+    def set_holdtime(self, val):
+        self.holdt = val
+
+    def set_delaytime(self, val):
+        self.delayt = val
+
+    def bufferclear(self):
+        self.visa.query('BC')
+
+#   System Mode commands; See the Keithley Programing manual for an overview
+    class sysmode:
+        class chmode(Enum):
+            SOURCE_VOLT = 1
+            SOURCE_CURR = 2
+            SOURCE_COMM = 3
+
+        class chfunc(Enum):
+            SWEEP = 1
+            STEP = 2
+            CONST = 3
+            SOURCE = 4
+
+        def __init__(self, device):
+            self.dev: Keithley4200A = device
+            self.channelA = None
+            self.channelB = None
+            self.delaytime = 0.0
+            self.holdtime = 0.0
+            self.autooff = True
+            self.waittime = 0.0
+            self.between = 0.01
+            self.integration = 2
+
+
+        def channelsetup(self, channel: int, name: str, mode: int, func: int):
+            self.dev.set_page(self.dev.Pages.CHANNEL_SET)
+            self.dev.query("CH{chan}, '{chan_name}V', '{chan_name}I, {chan_mode}, {chan_func}".format(chan = channel, chan_name = name, chan_mode = mode, chan_func = func))
+            if channel == 1:
+                self.channelA = (channel, name, mode, func)
+            if channel == 2:
+                self.channelB = (channel, name, mode, func)
+
+        def sweepsetup(self, channel: int, start: float, end: float, npoints: int, compliance: float):
+            assert 2*npoints <= 4096 # Limit for the 4200A
+
+            sweep_string = ""
+            sweep = np.linspace(start, end, npoints)
+            for step in sweep:
+                sweep_string = sweep_string + ", {}".format(float(step))
+            sweep = np.linspace(end, start, npoints)
+            for step in sweep:
+                sweep_string = sweep_string + ", {}".format(float(step))
+
+            self.dev.set_page(self.dev.Pages.SOURCE_SET)
+            mode = None
+            chan_set = None
+            if channel == 1:
+                chan_set = self.channelA
+            else:
+                chan_set = self.channelB
+
+            if chan_set[2] == self.chmode.SOURCE_VOLT:
+                mode = 'VL'
+            elif chan_set[2] == self.chmode.SOURCE_CURR:
+                mode = 'IL'
+
+            self.dev.query('{chan_mode}{chan_num}, 1, {compl}'.format(chan_mode = mode, chan_num = chan_set[0], compl = compliance) + sweep_string)
+
+            self.dev.query('DT {:.3f}'.format(self.delaytime))
+            self.dev.query('HT {:.1f}'.format(self.holdtime))
+
+        def measuresetup(self):
+            self.dev.set_page(self.dev.Pages.MEAS_SET)
+            self.dev.query('DM2')
+            self.dev.query('IN {:.2f}'.format(self.between))
+            self.dev.query('WT {:.3f}'.format(self.waittime))
+            self.dev.write_integT(self.integration)
+
+        def trigger(self):
+            self.dev.set_page(self.dev.Pages.MEAS_CON)
+            self.dev.query("ME1")
+
+        def retreve(self, channel):
+            chan_set = None
+            if channel == 1:
+                chan_set = self.channelA
+            else:
+                chan_set = self.channelB
+            self.dev.query("DO {}V".format(chan_set[1]))
+
+        def abort(self):
+
+            self.dev.set_page(self.dev.Pages.MEAS_CON)
+            self.dev.query("ME4")
+
+
+
+
